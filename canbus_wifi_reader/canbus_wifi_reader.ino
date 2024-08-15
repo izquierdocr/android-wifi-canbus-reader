9ff00f06238e76acd04e4ccd893ec88cf2390d3b
Ramon Izquierdo <izquierdocr@hotmail.com>
2024-08-14 21:25:22 -0600

    Testing CAN with Nano

--- canbus_wifi_reader/canbus_wifi_reader.ino
+++ canbus_wifi_reader/canbus_wifi_reader.ino
@@ -10,200 +10,162 @@
     by Ramon Izquierdo Cordova
 
 
+  Explanations about the CAN Bus
+  https://www.csselectronics.com/pages/can-bus-simple-intro-tutorial
+
+  ODB2 Codes
+  https://en.wikipedia.org/wiki/OBD-II_PIDs
+
 
 */
 
 // CAN bus libs -----------------------
-#include <Adafruit_MCP2515.h>
+#include <mcp2515.h>
 
 
 // CAN Bus vars -----------------------
-#define CS_PIN    10
-// Set CAN bus baud rate
-#define CAN_BAUDRATE (500000)
-#define MCP2515_CLOCK_FREQUENCY 8e6
 
-Adafruit_MCP2515 mcp(CS_PIN);
+#define CS_PIN    10
+
+MCP2515 mcp2515(CS_PIN);
 
 // program vars -----------------------
 
+// CAN Bus data types
 
-int readAnyPacket() {
-  // try to parse packet
-  int packetSize = mcp.parsePacket();
-
-  if (packetSize) {
-    // received a packet
-    Serial.print("Received ");
-
-    if (mcp.packetExtended()) {
-      Serial.print("extended ");
-    }
-
-    if (mcp.packetRtr()) {
-      // Remote transmission request, packet contains no data
-      Serial.print("RTR ");
-    }
-
-    Serial.print("packet with id 0x");
-    Serial.print(mcp.packetId(), HEX);
-
-    if (mcp.packetRtr()) {
-      Serial.print(" and requested length ");
-      Serial.println(mcp.packetDlc());
-    } else {
-      Serial.print(" and length ");
-      Serial.println(packetSize);
-
-      // only print packet data for non-RTR packets
-      while (mcp.available()) {
-        Serial.print((char)mcp.read());
-      }
-      Serial.println();
-    }
-
-    Serial.println();
-  }
-
-  int tempCAN;
-  tempCAN = random(MIN_TEMP_VALUE, MAX_TEMP_VALUE);
-  return tempCAN;
-}
+#define ENGINE_RPM 2
 
 
-int readRPM() {
-  mcp.beginPacket(0x7df, 8);
-  mcp.write(0x02);           // number of additional bytes
-  mcp.write(0x01);           // mode 01 - show current data
-  mcp.write(0x0c);           // engine RPM
-  mcp.endPacket();
-  // wait for response
-  while (mcp.parsePacket() == 0 ||
-         mcp.read() != 4 ||         // correct length
-         mcp.read() != 0x41 ||      // correct mode
-         mcp.read() != 0x0c);       // correct PID
-  // engine RPM = (A * 256 + B) / 4
-  float rpm = ((mcp.read() * 256.0) + mcp.read()) / 4.0;
-  Serial.print("Engine RPM = ");
-  Serial.println(rpm);
-  // ...
-  delay(1000);
-}
+int canValueType; // CAN Value type being read and shown
+
+bool gotReading = false; // flag for reading valid value from the CAN bus
 
 
-int readTemp(void) {
+static const unsigned long CHANGE_CAN_TYPE_INTERVAL = 10000; // ms
+static unsigned long lastChangeTime = 0;
 
-  // send packet: id is 11 bits, packet can contain up to 8 bytes of data
-  Serial.print("Sending packet ... ");
-/*
-  mcp.beginPacket(0x7df, 8);
-  mcp.write(0x02);           // number of additional bytes
-  mcp.write(0x01);           // mode 01 - show current data
-  mcp.write(0x05);           // engine coolant temperature
-  mcp.write(0x00);           // extra bytes
-  mcp.write(0x00);           // extra bytes
-  mcp.write(0x00);           // extra bytes
-  mcp.write(0x00);           // extra bytes
-  mcp.endPacket();
-*/
-  Serial.print("Here... ");
-  float engineTemp = 0;
+/*************************************************
+   Fucntions
+ *************************************************/
+
+// CAN Bus functions -----------------------
 
 
-  // wait for response
-/*
-  while (mcp.parsePacket() == 0 ||
-         mcp.read() != 4 ||         // correct length
-         mcp.read() != 0x41 ||      // correct mode
-         mcp.read() != 0x0c);       // correct PID
-  // engine RPM = (A * 256 + B) / 4
-  float rpm = ((mcp.read() * 256.0) + mcp.read()) / 4.0;
-*/
-
-/*
-  while (mcp.parsePacket() == 0 ||
-         mcp.read() != 3 ||         // correct length
-         mcp.read() != 0x41 ||      // correct mode
-         mcp.read() != 0x05);       // correct PID
-  // engine coolant temp = A - 40
-  float engineTemp = mcp.read() - 40;
-*/
-
-/*
-while (mcp.available()) {
-        Serial.print((char)mcp.read());
-}
-*/
-
-int packetSize = mcp.parsePacket();
-Serial.print("Size ");
-Serial.println(packetSize);
-if (packetSize) {
-    // received a packet
-    Serial.print("Received ");
-}
-  Serial.println("done");
-
-  delay(10);
-
-  /*
-  // send extended packet: id is 29 bits, packet can contain up to 8 bytes of data
-  Serial.print("Sending extended packet ... ");
-
-  mcp.beginExtendedPacket(0xabcdef);
-  mcp.write('w');
-  mcp.write('o');
-  mcp.write('r');
-  mcp.write('l');
-  mcp.write('d');
-  mcp.endPacket();
-
-  Serial.println("done");
-
-  delay(1000);
-  */
+struct can_frame setCANMsgData(void) {
+  struct can_frame canMsg;
   
-  //return (int)engineTemp;
-  return 80*packetSize;
+  switch (canValueType) {
+    case ENGINE_RPM:
+      canMsg.can_id  = 0x7df;
+      canMsg.can_dlc = 8;
+      canMsg.data[0] = 0x02;
+      canMsg.data[1] = 0x01;
+      canMsg.data[2] = 0x0C;
+      canMsg.data[3] = 0x00;
+      canMsg.data[4] = 0x00;
+      canMsg.data[5] = 0x00;
+      canMsg.data[6] = 0x00;
+      canMsg.data[7] = 0x00;
+      break;
+    default:
+      Serial.println("CAN Value type not supported");
+      break;
+  }
+  return canMsg;
 }
 
+
+float decodeCANData(struct can_frame canMsg) {
+  float canValue = 0.0;
+  
+  switch (canValueType) {
+    case ENGINE_RPM:
+      // Engine speed (rpm) = (A * 256 + B) / 4
+      canValue = (canMsg.data[3] * 256 + canMsg.data[4]) / 4;
+      break;
+    default:
+      Serial.println("CAN Value type not supported");
+      break;
+  }
+  return canValue;
+}
+
+
+float readCANValue(void) {
+  
+  struct can_frame canMsgSent = setCANMsgData();;
+  mcp2515.sendMessage(&canMsgSent);
+  //mcp2515.sendMessage(&canMsgSent);
+
+  float canValue = 0.0;
+  int loopCount = 0;
+  int maxLoopIterations = 500; //Trying to read answer n times
+  struct can_frame canMsgRead;
+  while (mcp2515.readMessage(&canMsgRead) == MCP2515::ERROR_OK) {
+    if ((canMsgRead.can_id >= 0x7e8 and canMsgRead.can_id <= 0x7ef) or loopCount>maxLoopIterations){
+      break;
+    }
+    loopCount = loopCount + 1;
+  }
+  if (canMsgRead.can_id >= 0x7e8 and canMsgRead.can_id <= 0x7ef and canMsgRead.data[1]==canMsgSent.data[1]+0x40 and canMsgRead.data[2]==canMsgSent.data[2] ) {
+    canValue = decodeCANData(canMsgRead);
+    gotReading = true;
+  }
+  else {
+    gotReading = false;
+  }
+  
+  return canValue;
+}
 
 
 void configureCAN() {
-  Serial.begin(9600); //(115200);
-  while(!Serial) delay(10);
-
-  Serial.println("MCP2515 Sender test!");
-
-  if (!mcp.begin(CAN_BAUDRATE)) {
-    Serial.println("Error initializing MCP2515.");
-    while(1) delay(10);
-  }
-
-  mcp.setClockFrequency(MCP2515_CLOCK_FREQUENCY);
+  Serial.println("Searching MCP2515...");
+  mcp2515.reset();
+  mcp2515.setBitrate(CAN_500KBPS,MCP_8MHZ);
+  mcp2515.setNormalMode();
   Serial.println("MCP2515 chip found");
+}
+
+void setCANValueType(int valueType) {
+  canValueType = valueType;
 }
 
 
 
+// Main fucntions -----------------------
+
 void setup() {
 
-  Serial.println('Setting CAN board');
-
+  Serial.begin(115200);
   configureCAN();
+  setCANValueType(ENGINE_RPM);
 }
 
 
 void loop() {
-  int tempCAN = 0;
-  //tempCAN = readTemp();
+  float canValue;
+  
+  canValue = readCANValue();
+  
+  //gotReading = true;
+  //canValue = random(0, 100);
+  
+  delay(1000); // Wait to data transfer from device to complete
+  
+  if (gotReading == true) {
+    Serial.print("Value from CAN Bus:");
+    Serial.println(canValue);
 
-  Serial.println(tempCAN);
-  showTempBar(tempCAN);
-
-  if(tempCAN>ALARM_TRIGGER_VALUE) {
-    Serial.println(F("Limit safe temperature reached. Stop engine inmediatly."));
-    playAlarm();
+    delay(500);
+    /*
+    unsigned long currentMillis = millis();
+    if(currentMillis - lastChangeTime >= CHANGE_CAN_TYPE_INTERVAL) {
+      lastChangeTime = currentMillis;
+      setCANValueType(nextCANType());
+      redraw=true;
+    }
+    */
   }
-  delay(500);
 }
+
